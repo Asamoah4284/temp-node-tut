ee50d70cce9166cda2895756752f6a636daec1d6
Asamoah4284 <richardasamoah284@gmail.com>
2023-11-12 23:59:55 +0000

    first commit

---
+++ fs-async.js
@@ -0,0 +1,34 @@
+const {readFile,writeFile} = require('fs')
+console.log('start');
+readFile('./content/first.txt','utf8',(err, result)=>{
+if(err){
+    console.log(err);
+    return;
+}
+const first = result
+readFile('./content/second.txt','utf8',(err,result)=>{
+    if(err){
+        console.log(err);
+        return;
+    }
+    const second = result
+    writeFile('./content/result-async.txt',
+    `Here is the result:${second}`,(err,result)=>{
+        if(err){
+            console.log('done with this task');
+            return
+        }
+    })
+})
+})
+console.log('starting next task');
+
+// Alternative method
+// const fs = require('fs')
+// fs.readFile('./content/first.txt','utf8',(err,result) =>{
+//     if(err){
+//     console.log(err);
+//     return;
+// }
+// console.log(result)
+// })
